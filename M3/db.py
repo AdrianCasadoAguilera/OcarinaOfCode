0d5c8924abb4e4ce05f4a288ec8923f48cd38147
AdrianCasadoAguilera <acasadoag2@gmail.com>
2024-01-09 19:55:26 +0100

    menu.py finalized
    Correction of mistakes and finalization of the menu

--- M3/db.py
+++ M3/db.py
@@ -3,6 +3,3 @@ import mysql.connector
 connection = mysql.connector.connect(user='OcarinaOfCode',password='1234',host='127.0.0.1',database='zelda')
 
 cur = connection.cursor()
-
-cur.execute("SELECT count(*) FROM game;")
-query = cur.fetchall()
